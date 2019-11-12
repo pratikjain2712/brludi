d4ac95d600f8b09d93261117d607714bab84b182
Pratik J <pratikjain2712@gmail.com>
2019-11-12 13:22:34 +0530

    added changes

--- assets/js/offCanvas.js
+++ assets/js/offCanvas.js
@@ -1,5 +1,6 @@
 $(function() {
   "use strict";
+  $(".offcanvas-collapse").toggleClass("open");
 
   $('[data-toggle="offcanvas"]').on("click", function() {
     $(".offcanvas-collapse").toggleClass("open");
