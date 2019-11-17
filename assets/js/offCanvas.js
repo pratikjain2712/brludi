dd8913ba42fcc1f77914df69250542b8d3119690
Pratik J <pratikjain2712@gmail.com>
2019-11-17 12:40:48 +0530

    updated the filters and the listing styles

--- assets/js/offCanvas.js
+++ assets/js/offCanvas.js
@@ -1,6 +1,5 @@
 $(function() {
   "use strict";
-  $(".offcanvas-collapse").toggleClass("open");
 
   $('[data-toggle="offcanvas"]').on("click", function() {
     $(".offcanvas-collapse").toggleClass("open");
